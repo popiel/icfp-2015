1b36e431e2c68c2aa55505e7607df62055d908ae
Alex Popiel <apopiel@aethericworlds.com>
2015-08-08 09:09:07 -0700

    Adding more output to the submission

--- submit_solution.sh
+++ submit_solution.sh
@@ -3,5 +3,6 @@
 git status -s | grep -s . && echo "Uncommitted changes and/or untracked files present.  Clean up your mess before submitting!" && exit 1
 
 git tag `date -u +'%Y-%m-%dT%H_%M_%SZ'`
-curl --user :`cat token` -X POST -H "Content-Type: application/json" -d @- https://davar.icfpcontest.org/teams/139/solutions
+curl --user :`cat token` -X POST -H "Content-Type: application/json" -d @- -i https://davar.icfpcontest.org/teams/139/solutions
+echo
 
