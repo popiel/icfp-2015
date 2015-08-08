65a7dc03e76a74b305afb50e2440d2885833f10d
Alex Popiel <apopiel@aethericworlds.com>
2015-08-08 09:01:37 -0700

    Trying again to submit

--- submit_solution.sh
+++ submit_solution.sh
@@ -2,6 +2,6 @@
 
 git status -s | grep -s . && echo "Uncommitted changes and/or untracked files present.  Clean up your mess before submitting!" && exit 1
 
-git tag `date -u +'%Y-%m-%dT%H:%M:%SZ'`
+git tag `date -u +'%Y-%m-%dT%H_%M_%SZ'`
 curl --user :`cat token` -X POST -H "Content-Type: application/json" -d @- https://davar.icfpcontest.org/teams/139/solutions
 
