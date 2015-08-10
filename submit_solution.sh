54b0a1145c99145330307d0c929a66f88ff1481c
Alex Popiel <apopiel@aethericworlds.com>
2015-08-10 03:17:48 -0700

    Final update for source submission.

--- submit_solution.sh
+++ submit_solution.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git status -s | grep -s . && echo "Uncommitted changes and/or untracked files present.  Clean up your mess before submitting!" && exit 1
+# git status -s | grep -s . && echo "Uncommitted changes and/or untracked files present.  Clean up your mess before submitting!" && exit 1
 
-git tag `date -u +'%Y-%m-%dT%H_%M_%SZ'`
+# git tag `date -u +'%Y-%m-%dT%H_%M_%SZ'`
 curl --user :`cat token` -X POST -H "Content-Type: application/json" -d @- -i https://davar.icfpcontest.org/teams/139/solutions
 echo
 
